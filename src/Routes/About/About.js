b7dfec6380472e19e5a47fa96675eec6404ccd16
shmiggy1010 <shmiggy1010@gmail.com>
2022-02-23 17:02:19 -0500

    format and spell check

--- src/Routes/About/About.js
+++ src/Routes/About/About.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-import portfolioImage from './PortfolioWebsites.png';
+import { Container, Row, Col, Card } from "react-bootstrap";
+import portfolioImage from "./PortfolioWebsites.png";
 
 class About extends React.Component {
   render() {
     return (
       <Container fluid className="pt-5">
         <Row className="justify-content-md-left">
-          <Col xs lg="4" className="pt-5">
+          <Col xs={9} className="pt-5">
             <h1>About me</h1>
             <br />
-            <h2>Over the past two years I have been the lead developer on 65 websites for the virtual event industry</h2>
+            <h2>
+              Over the past two years I have been the lead developer on 65
+              websites for the virtual event industry
+            </h2>
             <br />
-            <h3>I have developed supporting systems to reach automation goals and I have built and intagrated stand alone apps and backend systems to meet the features required for these websites. </h3>
+            <h3>
+              I have developed supporting systems to reach automation goals and
+              I have built and integrated stand alone apps and backend systems
+              to meet the features required for these websites.
+            </h3>
           </Col>
           <Col>
             <img className="img-fluid" src={portfolioImage} />
           </Col>
         </Row>
       </Container>
-    )
+    );
   }
 }
 

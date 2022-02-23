76ef1df7342ef45ba25f22f9b88c26b9f1fd8681
shmiggy1010 <shmiggy1010@gmail.com>
2022-02-23 17:02:33 -0500

    link style change

--- src/Components/NavigationHeader.js
+++ src/Components/NavigationHeader.js
@@ -1,30 +1,31 @@
-
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 class NavigationHeader extends React.Component {
-
-
-    render() {
-        return (
-            <div>
-                <Navbar bg="dark" variant="dark" expand="md" >
-                    <Navbar.Brand as={Link} to='/'>Luke Whittle</Navbar.Brand>
-                    <Nav.Link as={Link} to="/">
-                        Home
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/About">
-                        About
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/Contact">
-                        Contact
-                    </Nav.Link>
-                </Navbar>
-                <Outlet />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark" expand="md">
+          <Navbar.Brand style={{ color: "white" }} as={Link} to="/">
+            Luke Whittle
+          </Navbar.Brand>
+          <Nav.Link style={{ color: "white" }} as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} as={Link} to="/About">
+            About
+          </Nav.Link>
+          <Nav.Link style={{ color: "white" }} as={Link} to="/Contact">
+            Contact
+          </Nav.Link>
+        </Navbar>
+        <Outlet />
+      </div>
+    );
+  }
 }
 
 export default NavigationHeader;

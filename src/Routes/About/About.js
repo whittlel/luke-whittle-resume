3f9ca8784fe5ff00009d79accb5f2da6c4fff00a
Luke <shmiggy1010@gmail.com>
2022-02-23 13:00:59 -0500

    About page refactor and formatting start

---
+++ src/Routes/About/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+import portfolioImage from './PortfolioWebsites.png';
+
+class About extends React.Component {
+  render() {
+    return (
+      <Container fluid className="pt-5">
+        <Row className="justify-content-md-left">
+          <Col xs lg="4" className="pt-5">
+            <h1>About me</h1>
+            <br />
+            <h2>Over the past two years I have been the lead developer on 65 websites for the virtual event industry</h2>
+            <br />
+            <h3>I have developed supporting systems to reach automation goals and I have built and intagrated stand alone apps and backend systems to meet the features required for these websites. </h3>
+          </Col>
+          <Col>
+            <img className="img-fluid" src={portfolioImage} />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default About;
